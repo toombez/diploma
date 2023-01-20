51c26fdf739b3815fd2607359b76bc591604cd4a
Timur <timurtokaev.toombez@gmail.com>
2023-01-20 04:27:47 +0300

    Remove Vector types

--- packages/problem/src/types.ts
+++ packages/problem/src/types.ts
@@ -1,19 +1,15 @@
-export type Vector<T> = Array<T>
-export type Vector2D<T> = Vector<Vector<T>>
-export type Vector3D<T> = Vector2D<Vector<T>>
-
 export interface IOperation {
     duration: number
 }
 
 export interface ITask {
-    operations: Vector<IOperation>
+    operations: IOperation[]
 }
 
 export interface IMachine {
-    tasks: Vector<ITask>
+    tasks: ITask[]
 }
 
 export interface IProblem {
-    machines: Vector<IMachine>
+    machines: IMachine[]
 }
