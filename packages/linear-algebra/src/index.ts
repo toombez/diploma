544befbb867c35f82eba1d61db1f75a456c45491
Timur <timurtokaev.toombez@gmail.com>
2023-01-20 04:19:46 +0300

    Add playground

---
+++ packages/linear-algebra/src/index.ts
@@ -0,0 +1,7 @@
+import Vector from './structures/Vector'
+import Vector2 from './structures/Vector2'
+
+export {
+    Vector,
+    Vector2,
+}
--- packages/linear-algebra/src/playground/main.ts
+++ packages/linear-algebra/src/playground/main.ts
@@ -1,1 +1,9 @@
-console.log('jmgkwag');
+import { Vector2 } from '../index'
+
+const v = new Vector2([[1, 2]])
+const tv = v.transposed()
+
+console.log(v.rawValues)
+console.log(tv.rawValues)
+console.log(v.transpose().rawValues)
+
