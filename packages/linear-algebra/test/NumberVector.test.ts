027e4814e323e913a32029b4424cedb9941f5193
Timur <timurtokaev.toombez@gmail.com>
2023-01-24 20:25:05 +0300

    Add tests for NumberVector

---
+++ packages/linear-algebra/test/NumberVector.test.ts
@@ -0,0 +1,32 @@
+import { NumberVector } from '../src'
+
+describe('NumberVector structure', () => {
+    it('Vector length', () => {
+        const rawVector = [3, 4, 5]
+        const vector = new NumberVector([3, 4, 5])
+
+        const length = Math.sqrt(
+            Math.pow(rawVector[0], 2)
+            + Math.pow(rawVector[1], 2)
+            + Math.pow(rawVector[2], 2)
+        )
+
+        expect(vector.length).toBe(length)
+    })
+
+    it('Negate vector method returns negative direction vector', () => {
+        const vector = new NumberVector([1, 2, 3])
+        const negativeVector = vector.negate()
+
+        const negativeComponents = vector.components.map(c => c * (-1))
+
+        expect(negativeVector.components).toStrictEqual(negativeComponents)
+    })
+
+    it('Sum of normalized vector components equals 1', () => {
+        const vector = new NumberVector([1, 2])
+        const normalizedVector = vector.normalize()
+
+        expect(normalizedVector.length).toBe(1)
+    })
+})
