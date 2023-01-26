a19da5e028fa118e49d1b417ddd48f5ce8761e66
Timur <timurtokaev.toombez@gmail.com>
2023-01-26 18:40:24 +0300

    Change Task

--- packages/problem/src/models/Task.ts
+++ packages/problem/src/models/Task.ts
@@ -1,16 +1,53 @@
 import { Vector } from '@diploma/linear-algebra'
-import { IRawTask } from "../types"
+import Machine from './Machine'
 import Operation from "./Operation"
 
+/**
+ * Model for problem's task
+ */
 export default class Task {
-    public get operations() {
-        return this._operations
+    /**
+     * Map for machines and operations
+     */
+    public operations = new Map<Machine, Operation>
+
+    /**
+     * Task operation durations vector
+     */
+    public get durationsVector() {
+        return new Vector([...this.operations.values()])
     }
 
-    private _operations: Vector<Operation>
+    /**
+     * Create task from map
+     *
+     * @param map map of machines and operations
+     * @returns task
+     */
+    public static fromMap(map: Map<Machine, Operation>) {
+        const task = new Task()
+        task.operations = map
 
-    constructor(options: IRawTask) {
-        const operations = options.operations.map(o => new Operation(o))
-        this._operations = new Vector(operations)
+        return task
+    }
+
+    /**
+     * Create task from durations array
+     *
+     * @param durations durations array
+     * @returns new task, created machines and operations
+     */
+    public static fromDurations(durations: number[]) {
+        const operationsMap = new Map(durations.map(duration => {
+            return [new Machine(), new Operation({ duration })]
+        }))
+
+        const task = this.fromMap(operationsMap)
+
+        return {
+            task,
+            machines: operationsMap.keys(),
+            operations: operationsMap.values(),
+        }
     }
 }
